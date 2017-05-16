f8b461f23e17274dbef81dcfbd20662ec9a894da
Gakuto Fujioka <g-fujioka@rhizome-e.com>
2017-05-16 11:43:26 +0900

    Use SSL and the Puma webserver in production

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -11,8 +11,10 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                          password_confirmation: "bar" } }
     end
     assert_template 'users/new'
+    assert_select 'form[action="/signup"]'
+    assert_select 'div#error_explanation'
+    assert_select 'div.field_with_errors'
   end
-
 
   require 'test_helper'
 
